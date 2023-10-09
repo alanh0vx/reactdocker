9175e736cca1934f60ef71277dc6ca639a7a644e
alanh0vx <alanh0@vxrl.hk>
2023-10-09 15:17:28 +0100

    Test

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Auto Deploy , Hello world!!
+          Auto Deploy Test, Hello world!!
         </p>
       </header>
     </div>
